9929759b9d4df0269b1db7dcfe58c0f762bec209
Dr Z <zamore@vt.edu>
2021-11-28 15:51:26 -0700

    how to use the soundResponse library (import)

--- Sound library/soundResponse_example.py
+++ Sound library/soundResponse_example.py
@@ -3,7 +3,20 @@
 """
 Created on Wed Jul  1 20:18:09 2020
 
-@author: master
+@author: Dr. Z
+
+Demonstration of how to use soundResponse.py as an imported library.
+soundResponse.py MUST be in the same folder as your file and
+    MUST be submitted with your code!
+    
+This demonstration uses a soundResponse Ctrlr object to play music based on keystroke. ('s' key will start and stop the music)
+You can use this code to:
+    1. Understand how to use the soundResponse class. Amplitude (loudness) is shown in this example, but you can do a similar task
+    with frequency (pitch) by using music.getCurrFreq()[0]
+    2. Understand how to overwrite callback functions so that the same keypress will do 2  things. (See lines 45, 68, and 70)
+    
+PROTIP: If imported, this borrowed code only counts as 1 line of outside code.
+
 """
 
 import soundResponse as SR
@@ -29,6 +42,8 @@ def stop():
     '''callback function that stops animation while it's running'''
     global run
     run = False
+    panel.onkey(go,'s') # change the function of the s key press  back to go
+
 
 def go():
     '''Callback function that plays sound and does entire animation'''
@@ -50,9 +65,9 @@ def go():
         # quit the while loop when the song ends
         if amp ==-1:
             run = False
-        panel.onkey(stop,'s')
+        panel.onkey(stop,'s') # change the function of the s key press.
 
-panel.onkey(go,'s')
-turtle.listen()
+panel.onkey(go,'s')  # start off with one function for the s key  press...
+turtle.listen() # required for key presses
         
 turtle.done()
