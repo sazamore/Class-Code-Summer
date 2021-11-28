bcfda9c91fe955926e39d7b30a84c08b0474e552
Dr Z <zamore@vt.edu>
2021-11-28 15:37:38 -0700

    How to move multiple turtles at once

--- synchroTurtles.py
+++ synchroTurtles.py
@@ -4,27 +4,30 @@
 Created on Thu Sep 17 14:48:01 2020
 
 @author: Dr. Z
+
 Draw squares in opposite directions at the same time.
 """
 
-from turtle import *
+import turtle
 
-LEFT = Turtle() #turtle draws square to left
-RIGHT = Turtle() # draws to right
+LEFT = turtle.Turtle() #turtle draws square to left
+RIGHT = turtle.Turtle() # draws to right
 LEFT.speed(10)
 RIGHT.speed(10)
 
-for i in range(2):    
-    
-    
-    for k in range(4):
-        # draw squares
-        RIGHT.forward(50)
-        LEFT.forward(50)
-        RIGHT.right(90)
-        LEFT.left(90)
-    
-    LEFT.forward(100)
-    RIGHT.forward(100)
+running = True
+
+while running:
+    for i in range(2):    
+        for k in range(4):
+            # draw squares
+            RIGHT.forward(50)
+            RIGHT.right(90)
+            LEFT.forward(50)
+            LEFT.left(90)
+
+        LEFT.forward(100)
+        RIGHT.forward(100)
+    running = False # animation complete
         
-    
+turtle.done()
