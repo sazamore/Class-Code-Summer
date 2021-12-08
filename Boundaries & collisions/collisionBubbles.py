ab38a130ca985e9880a177c9b3ce3a4877197234
Dr Z <zamore@vt.edu>
2021-12-08 14:25:39 -0700

    fixed list collision error

--- Boundaries & collisions/collisionBubbles.py
+++ Boundaries & collisions/collisionBubbles.py
@@ -54,9 +54,9 @@ def isCollision(turt,target,buffer=30):
             targX = target[i].xcor()
             targY = target[i].ycor()
             if round(targX)-buffer<=round(x)<=round(targX)+buffer and round(targY)-buffer<=round(y)<=round(targY)+buffer:
-                return True
-            else:
-                return False
+                return True # collision found? return a True and stop running the method.
+        return False # no collision found? Return a False
+      
     elif type(target)== turtle.Turtle:
         # If it's a turtle, get its position and checks for collision
         targX = target[i].xcor()
